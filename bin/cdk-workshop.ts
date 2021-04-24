ea99225cabe224a59ac22bfedd2d5ef04ad10d78
NKStupid <64253633+NKStupid@users.noreply.github.com>
2021-04-24 12:09:09 +0800

    Update cdk-workshop.ts

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import { CodeStack } from '../lib/code-stack';
+
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new CdkWorkshopStack(app, 'CodeStack');
