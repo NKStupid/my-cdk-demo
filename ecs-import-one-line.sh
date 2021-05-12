b400e12926d322ad74e65c9287f4cd18577c402e
NKStupid <64253633+NKStupid@users.noreply.github.com>
2021-05-12 14:57:10 +0800

    Update ecs-import-one-line.sh

--- ecs-import-one-line.sh
+++ ecs-import-one-line.sh
@@ -1,3 +1,5 @@
+# export the task def
+family='fargate-task'
 # one-liner
 aws ecs describe-task-definition --task-definition $family \
 --query "taskDefinition.{family:family, taskRoleArn:taskRoleArn, executionRoleArn:executionRoleArn, networkMode:networkMode, containerDefinitions:containerDefinitions, volumes:volumes, placementConstraints:placementConstraints, requiresCompatibilities:requiresCompatibilities, cpu:cpu, memory:memory, tags:tags, pidMode:pidMode, ipcMode:ipcMode, proxyConfiguration:proxyConfiguration}" | jq 'del(.[] | nulls)' | xargs -0 aws ecs register-task-definition --family newFamily --cli-input-json
